2367eb1a690eef683b0c2df78935d6a29710212c
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-07-03 10:13:35 +0700

    recreate migrations

---
+++ database/migrations/2020_07_03_003203_create_question_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date_created', 0);
+            $table->string('title');
+            $table->text('content');
+            $table->integer('like_num')->default(0);
+            $table->integer('dislike_num')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+}
